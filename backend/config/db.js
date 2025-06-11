d66aa8f80c64fc3ad4fe49957a434cccfafea251
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2025-06-11 17:31:03 -0400

    Set up back-end & first model

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB connected at: ${conn.connection.host}`)
+        process.exit(0);
+    } catch (err) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
