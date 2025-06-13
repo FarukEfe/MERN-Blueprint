9c2e3aab4d7bd769c383e6304767798f9c715fa4
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2025-06-12 20:15:49 -0400

    create page and navbar

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,58 @@
+import { Box, Container, Heading, useColorModeValue, VStack } from "@chakra-ui/react";
+
+import { useState } from "react";
+
+
+const CreatePage = () => {
+    
+    const [newProduct, useProduct] = useState({
+        name: "",
+        price: "",
+        image: ""
+    })
+
+    const handleAddProduct = () => {
+        console.log(newProduct)
+    }
+
+    return <Container>
+        <VStack
+            spacing={8}
+        >
+            <Heading as={'h1'} size={'2xl'} textAlign={'center'} mb={8}>
+                Create New Product
+            </Heading>
+
+            <Box
+                w={'full'} bg={useColorModeValue('white', 'gray.800')}
+                p={6} rounded={'lg'} shadow={'md'}
+            >
+                <Vstack spacing={4}>
+                    <Input
+                        placeholer='Product Name'
+                        name='name'
+                        value={newProduct.name}
+                        onChange={(e) => useProduct({ ...newProduct, name: e.target.value }) }
+                    />
+                    <Input
+                        placeholer='Product Price'
+                        name='price'
+                        value={newProduct.name}
+                        onChange={(e) => useProduct({ ...newProduct, price: e.target.value }) }
+                    />
+                    <Input
+                        placeholer='Product Image'
+                        name='image'
+                        value={newProduct.name}
+                        onChange={(e) => useProduct({ ...newProduct, image: e.target.value }) }
+                    />
+                    <Button colorScheme='blue' onClick={handleAddProduct} w='full'>
+                        Add Product
+                    </Button>
+                </Vstack>
+            </Box>
+        </VStack>
+    </Container>
+};
+
+export default CreatePage;
